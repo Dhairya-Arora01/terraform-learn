c0103b2c742d219025a50f55528476d758da306e
Dhairya-Arora01 <dhairyarora0208in@gmail.com>
2023-06-22 22:58:34 +0530

    Isolating state with code structure

---
+++ structuring/global/providers/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 0.12"
+}
+
+provider "azurerm" {
+  features{}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "structural"
+  location = "westus"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "dhairyasa02"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "statefiles"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
